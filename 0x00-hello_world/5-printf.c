f4bb94041a5a9fea92022bad322cc11ba18a13a6
fablib250 <youremail@example.com>
2023-06-17 12:35:55 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a c program that print a l u f function of  printf
+ * return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
