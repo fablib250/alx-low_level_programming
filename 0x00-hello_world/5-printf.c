b6ffde3991bf7bbc8f4b3335acce23b7bdd39a65
fablib250 <youremail@example.com>
2023-06-17 14:04:07 -0700

    6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
- * main - a c program that print a l u f function of  printf
+ * main - Entry point of the program
  * return: 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-	return (0);
-}
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
