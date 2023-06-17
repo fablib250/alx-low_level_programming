b46be7fbfb3cc13fdfaef015ed49e3f41da7e28d
fablib250 <youremail@example.com>
2023-06-17 12:14:52 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that pt a line with puts fns
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
